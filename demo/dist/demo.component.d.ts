05d94432443060efa2804bd6d51ab62da185deb0
crisbeto <crisbeto@abv.bg>
2020-07-22 12:33:26 +0200

    Update 2020-07-22T10:33:14.384Z

---
+++ demo/dist/demo.component.d.ts
@@ -0,0 +1,30 @@
+import { RoundProgressEase } from 'round-progress';
+export declare class DemoComponent {
+    current: number;
+    max: number;
+    stroke: number;
+    radius: number;
+    semicircle: boolean;
+    rounded: boolean;
+    responsive: boolean;
+    clockwise: boolean;
+    color: string;
+    background: string;
+    duration: number;
+    animation: string;
+    animationDelay: number;
+    animations: string[];
+    gradient: boolean;
+    realCurrent: number;
+    constructor(ease: RoundProgressEase);
+    increment(amount?: number): void;
+    getOverlayStyle(): {
+        top: string;
+        bottom: string;
+        left: string;
+        transform: string;
+        '-moz-transform': string;
+        '-webkit-transform': string;
+        'font-size': string;
+    };
+}
